f9d3c6da856f0463fba3ef6ef463a9f9f7ceb89c
vaishnavika <77280301+vaishnavika@users.noreply.github.com>
2022-03-28 17:34:45 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -1,13 +1,20 @@
 player1_name=localStorage.getItem("player1_name");
 player2_name=localStorage.getItem("player2_name");
-player1_score=0;
-player2_score=0;
-document.getElementById("player1_name").value=player1_name+":";
-document.getElementById("player2_name").value=player2_name+":";
-document.getElementById("player1_name").value=player1_score+":";
-document.getElementById("player2_name").value=player2_score+":";
 
+document.getElementById("player1_name").innerHTML=player1_name+":";
+document.getElementById("player2_name").innerHTML=player2_name+":";
 
+question_turn="player1";
+answer_turn="player2";
+
+score_1=0;
+score_2=0;
+
+document.getElementById("score_1").innerHTML=score_1;
+document.getElementById("score_2").innerHTML=score_2;
+
+document.getElementById("player_question").innerHTML="Question Turn - "+player1_name;
+document.getElementById("player_answer").innerHTML="Answer Turn - "+player2_name;
 
 funcion send(){
     number1=document.getElementById("number1").value;
